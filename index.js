02c077ceb3bd5014f4fcdc2c532461f7736e4eac
cdecu <carlos@decumont.be>
2017-06-23 01:44:18 +0200

    service-2-service Test passed !

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
+#!/usr/bin/env node
 
-
-var fs = require('fs');
-var readline = require('readline');
 var google = require('googleapis');
-var googleAuth = require('google-auth-library');
 var drive = google.drive('v3');
 
 
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/drive-nodejs-quickstart.json
 var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'drive-nodejs-quickstart.json';
 
 // Load client secrets from a local file.
+// to get the public key openssl rsa -in test.txt -pubout
 var key = require('./client_secret.json');
+
+// Build the jwt
 var jwtClient = new google.auth.JWT(
     key.client_email,
     null,
@@ -23,12 +19,12 @@ var jwtClient = new google.auth.JWT(
     "cdecu@rmxgcp.com"
 );
 
+
 jwtClient.authorize(function (err, tokens) {
     if (err) {
         console.log(err);
         return;
         }
-    // console.log(JSON.stringify(jwtClient, null, '  '));
     // Make an authorized request to list Drive files.
     var req = drive.files.list({auth: jwtClient}, function (err, resp) {
         if (err) {
@@ -37,6 +33,5 @@ jwtClient.authorize(function (err, tokens) {
             }
         console.log(JSON.stringify(resp, null, '  '));
         });
-    // console.log(req);
 });
 
